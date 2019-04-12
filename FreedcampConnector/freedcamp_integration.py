8a8839639386c58f4f3ce97fded7682c5597e667
Jiří Pešík <Jiri.Pesik@aimtec.cz>
2019-04-12 22:14:40 +0200

    API connection refactoring
    resolved #3

--- FreedcampConnector/freedcamp_integration.py
+++ FreedcampConnector/freedcamp_integration.py
@@ -5,24 +5,32 @@ import urllib3
 import hmac
 import json
 
-jsonConfig = common.jsonConfig
+def get_address(address_name):
+    with open("freedcamp_urls.json", encoding="UTF-8") as f:
+        jsonFreddcampURLs = json.load(f, encoding="utf8")
+    return jsonFreddcampURLs[address_name]
 
-FreedcampAPIKey = jsonConfig["Freedcamp"]["FreedcampAPIKey"]
-FreedcampAPISecret = jsonConfig["Freedcamp"]["FreedcampAPISecret"]
+def generate_security_address():
+    jsonConfig = common.jsonConfig
+    FreedcampAPIKey = jsonConfig["Freedcamp"]["FreedcampAPIKey"]
+    FreedcampAPISecret = jsonConfig["Freedcamp"]["FreedcampAPISecret"]
+    unixTimestamp = str(round(time.time(), 0))
+    hashed = hmac.new(bytes(FreedcampAPISecret, 'UTF-8'), bytes(FreedcampAPIKey + unixTimestamp, 'UTF-8'), hashlib.sha1).hexdigest()
+    UrlSecurityString = "?api_key=" + FreedcampAPIKey + "&timestamp=" + unixTimestamp + "&hash=" + hashed
+    return UrlSecurityString
 
-unixTimestamp = str(round(time.time(), 0))
-hashed = hmac.new(bytes(FreedcampAPISecret, 'UTF-8'), bytes(FreedcampAPIKey + unixTimestamp, 'UTF-8'), hashlib.sha1).hexdigest()
+def call_freedcamp_API(url):
+    http = urllib3.PoolManager()
+    response = http.request("GET", url)
+    return json.loads(response.data.decode('utf-8'))
 
-UrlSecurityString = "?api_key=" + FreedcampAPIKey + "&timestamp=" + unixTimestamp + "&hash=" + hashed
+def load_projects():
+    url = get_address("projects") + generate_security_address()
+    projects_call_response = call_freedcamp_API(url)
+    for project in projects_call_response["data"]["projects"]:
+        projectID = project["project_id"]
+        projectName = project["project_name"]
+        output = "{0} {1} \n".format(projectID, projectName)
+        print(output)
 
-url = "https://freedcamp.com/api/v1/projects/" + UrlSecurityString
-
-http = urllib3.PoolManager()
-response = http.request("GET", url)
-responseJson = json.loads(response.data.decode('utf-8'))
-
-for project in responseJson["data"]["projects"]:
-    projectID = project["project_id"]
-    projectName = project["project_name"]
-    output = "{0} {1} \n".format(projectID, projectName)
-    print(output)
+load_projects()
