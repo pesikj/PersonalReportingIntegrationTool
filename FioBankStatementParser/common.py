4f3bbaf006385908f3688df5eb1a146bd2531aab
Jiří Pešík <jiri.pesik@outlook.cz>
2020-02-01 13:46:56 +0100

    New Transaction Type

--- FioBankStatementParser/common.py
+++ FioBankStatementParser/common.py
@@ -2,7 +2,7 @@ import json
 import logging
 import azure.cosmos.cosmos_client as cosmos_client
 
-with open("Config.json", encoding="UTF-8") as f:
+with open("config.json", encoding="UTF-8") as f:
     jsonConfig = json.load(f, encoding="utf8")
 
 logging.basicConfig(level=logging.INFO)
