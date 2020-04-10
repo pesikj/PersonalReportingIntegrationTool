afab905f886a8b3d096793dc72b4bcd22c5420ea
Jiří Pešík <jiri.pesik@gmail.com>
2020-04-10 20:04:00 +0200

    Added new transaction type

--- FioBankStatementParser/main.py
+++ FioBankStatementParser/main.py
@@ -2,4 +2,5 @@ import parse_statement as parst
 import buxfer_integration as bux
 import common
 
-parst.CopyTransactionFromBankToBuxfer()
+#parst.CopyTransactionFromBankToBuxfer()
+bux.UploadSpecificTransaction(22989122397)
