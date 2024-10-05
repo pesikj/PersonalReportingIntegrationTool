d8d6619b8f5cc98883ba339c6080d6c584a401ef
Jiří Pešík <jiri.pesik@gmail.com>
2024-10-05 17:25:33 +0200

    Delete GarminConnectedConnector directory

--- GarminConnectedConnector/common.py
+++
@@ -1,8 +0,0 @@
-import json
-import logging
-
-with open("config.json", encoding="UTF-8") as f:
-    jsonConfig = json.load(f)
-
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
